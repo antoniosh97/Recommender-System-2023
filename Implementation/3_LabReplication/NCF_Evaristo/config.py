4a1706b24cd5a2bb6264462e14d0043a5e81fb95
Evaristo <evbroul@gmail.com>
2023-03-07 09:29:38 +0100

    NCF Test from paper

---
+++ Implementation/3_LabReplication/NCF_Evaristo/config.py
@@ -0,0 +1,8 @@
+DATA_URL = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
+
+MAIN_PATH = '/Users/ebc/Documents/_Private/_Trabalho/Estudos/AI-UPC/08-Project/01 - Project Amazon/Recommender-System-2023/Implementation/3_LabReplication/NCF_Evaristo/'
+
+DATA_PATH = MAIN_PATH + 'data/ml-1m/ratings.dat'
+MODEL_PATH = MAIN_PATH + 'models/'
+
+MODEL = 'ml-1m_Neu_MF'
