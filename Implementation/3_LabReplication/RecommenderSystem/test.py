30199d27bbdadf6af07f859d395c7bd4ff5d0584
Evaristo <evbroul@gmail.com>
2023-03-08 17:26:32 +0100

    Cleaning test.py

--- Implementation/3_LabReplication/RecommenderSystem/test.py
+++
@@ -1,9 +0,0 @@
-import random
-import numpy as np
-
-sorted_data = np.ndarray([])
-data = np.ndarray([])
-sorted_data = np.random.randint(low=1, high=101, size=1)
-print(sorted_data)
-data = sorted_data[-1]#[:-1]
-print(data)
