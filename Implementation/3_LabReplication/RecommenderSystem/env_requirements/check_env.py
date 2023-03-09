d38589ca8fad04cea56035dddec544599920b3ac
Evaristo <evbroul@gmail.com>
2023-03-09 16:55:09 +0100

    Check your environment and install missing packages

--- Implementation/3_LabReplication/RecommenderSystem/env_requirements/check_env.py
+++ Implementation/3_LabReplication/RecommenderSystem/env_requirements/check_env.py
@@ -2,15 +2,11 @@ import pkg_resources
 import required_libs as l
 import subprocess
 
-#Check you environment and install missing packages
+#Check your environment and install missing packages
 
 installed_packages = pkg_resources.working_set
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-my_libs = []
-for value in installed_packages_list:
-    my_libs.append(value)
-
-diff = [pac for pac in l.required_libs if pac not in my_libs]
+diff = [pac for pac in l.required_libs if pac not in installed_packages_list]
 
 if len(diff) > 0:
     print(diff)
