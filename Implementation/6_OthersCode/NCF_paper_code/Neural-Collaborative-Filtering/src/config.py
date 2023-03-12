e18d35f934c7de727d69f09ed3f9ae9c18058646
Joan Dayas <101429909+joandayas@users.noreply.github.com>
2023-03-12 12:07:36 +0100

    Add files via upload

---
+++ Implementation/6_OthersCode/NCF_paper_code/Neural-Collaborative-Filtering/src/config.py
@@ -0,0 +1,9 @@
+DATA_URL = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
+
+MAIN_PATH = 'Z:/Personal/UPC_POSTGRADO_IA_DL/9-PROJECT/Neural_Collaborative_Filtering/'
+
+DATA_PATH =  'data/ml-1m/ratings.dat'
+DATA_PATH2 =  'data/ml-1m/ratings6_6.csv'
+MODEL_PATH = 'models/'
+
+MODEL = 'ml-1m_Neu_MF'
